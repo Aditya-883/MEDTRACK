1a928685c0646e0c870aaa00f40d2c16ab52d150
AlphaCoded <adityajoshi1491@gmail.com>
2026-04-13 14:14:05 +0530

    fixed frontend

--- client/src/app/layout.js
+++ client/src/app/layout.js
@@ -1,43 +1,19 @@
 import './globals.css';
-import Navbar from '../components/Navbar';
-import Footer from '../components/Footer';
 import WalletListener from './WalletListener';
-import Sidebar from '../components/layout/Sidebar';
 
 export const metadata = {
   title: 'MedTrack',
-  description: 'Healthcare DApp',
+  description: 'Blockchain-secured Healthcare DApp',
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className="bg-gray-100">
-
+      <body className="bg-gray-100 dark:bg-gray-900">
         <WalletListener />
-
-        {/* Sidebar */}
-        <div className="hidden md:block">
-          <Sidebar />
-        </div>
-
-        {/* Navbar */}
-        <div className="fixed top-0 left-16 w-[calc(100%-4rem)] h-16 z-30">
-          <Navbar />
-        </div>
-
-        {/* Content */}
-        <div className="pt-20 ml-16 px-6 min-h-screen flex flex-col">
-          <main className="flex-1">
-            {children}
-          </main>
-        </div>
-
-        {/*Footer*/}
-        <div className="ml-16 w-[calc(100%-4rem)]">
-          <Footer />
-        </div>
-
+        <main className="min-h-screen">
+          {children}
+        </main>
       </body>
     </html>
   );
