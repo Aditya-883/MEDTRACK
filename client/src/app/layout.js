a7bbb96286ab2cea6e928613c4720f00a2c9ae54
AlphaCoded <adityajoshi1491@gmail.com>
2026-04-13 14:46:11 +0530

    Update layout.js

--- client/src/app/layout.js
+++ client/src/app/layout.js
@@ -1,8 +1,4 @@
 import './globals.css';
-import Navbar from '../components/Navbar';
-import Footer from '../components/Footer';
-import WalletListener from './WalletListener';
-import Sidebar from '../components/layout/Sidebar';
 
 export const metadata = {
   title: 'MedTrack',
@@ -13,31 +9,9 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className="bg-gray-100">
-
-        <WalletListener />
-
-        {/* Sidebar */}
-        <div className="hidden md:block">
-          <Sidebar />
-        </div>
-
-        {/* Navbar */}
-        <div className="fixed top-0 left-16 w-[calc(100%-4rem)] h-16 z-30">
-          <Navbar />
-        </div>
-
-        {/* Content */}
-        <div className="pt-20 ml-16 px-6 min-h-screen flex flex-col">
-          <main className="flex-1">
-            {children}
-          </main>
-        </div>
-
-        {/*Footer*/}
-        <div className="ml-16 w-[calc(100%-4rem)]">
-          <Footer />
-        </div>
-
+        <main className="min-h-screen">
+          {children}
+        </main>
       </body>
     </html>
   );
